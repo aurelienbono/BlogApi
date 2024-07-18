0f83e59d694eabb9f7deaff034a85207de6f448c
aurelienbono <bonombelleaurelien08@gmail.com>
2024-07-18 18:38:34 +0000

    adds routes and readme

---
+++ README.md
@@ -0,0 +1,60 @@
+# Blog API
+
+This project is a Blog API built with FastAPI. It provides endpoints for creating, reading, updating, and deleting blog posts. The API follows the OpenAPI 3.1 specification.
+
+### Features
+- Create a new blog post
+- Retrieve all blog posts
+- Retrieve a specific blog post by ID
+- Update an existing blog post
+- Delete a blog post by ID
+Endpoints
+
+### Rootes
+*GET /*: Root endpoint to check if the API is running.
+Blog Operations
+*GET /api/v0/all_blog:* Retrieve all blog posts.
+*GET /api/v0/blog/{str_id}:* Retrieve a specific blog post by its ID.
+*POST /api/v0/create_post:* Create a new blog post.
+*DELETE /api/v0/del_blog/{str_id}:* Delete a blog post by its ID.
+*PUT /api/v0/update_blog/{str_id}:* Update an existing blog post by its ID.
+
+## Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/yourusername/blog-api.git
+
+cd blog-api
+```
+
+## Usage
+
+```python
+source venv/bin/activate  # On Windows, use `venv\Scripts\activate`
+
+# Install the dependencies:
+pip install -r requirements.txt
+
+# Run the application:'
+py src/run.py
+
+# returns 'phenomenon'
+foobar.singularize('phenomena')
+```
+### Schemas
+BlogBase
+Defines the basic structure of a blog post.
+
+### Usage
+Once the server is running, you can access the API documentation at - - -- 
+- http://127.0.0.1:8000/docs 
+- http://127.0.0.1:8000/redoc.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+## License
---
+++ src/routes/routes.py
@@ -0,0 +1,89 @@
+from fastapi import APIRouter , Depends , File , UploadFile
+
+from schema.schema import BlogBase , UploadFiles
+from models.models import SessionLocal, Blog 
+from sqlalchemy.orm import Session
+from datetime import datetime
+import shutil
+
+api_router = APIRouter()
+
+
+VERSION = 0
+
+
+def get_db(): 
+    
+    db = SessionLocal()
+    
+    try : 
+        yield db 
+    finally : 
+        db.close()
+
+
+
+@api_router.get('/')
+def root(): 
+    return {"message":"Hello world"}
+
+
+@api_router.get(f'/api/v{VERSION}/all_blog')
+def get_all_blogs(db:Session = Depends(get_db)): 
+    all_blogs = db.query(Blog).all()
+    return all_blogs
+
+@api_router.get(f'/api/v{VERSION}/blog/{{str_id}}')
+def get_blogs( str_id:str, db: Session = Depends(get_db)): 
+    blog_infos = db.query(Blog).filter(Blog.id==str_id).first()
+    if blog_infos : 
+        return blog_infos 
+    return {'id': str_id, 'message': 'Blog Not Found'} 
+    
+@api_router.post(f'/api/v{VERSION}/create_post')
+def create_post(blog: BlogBase, db: Session = Depends(get_db)): 
+    blog_info = Blog(
+        id=blog.id,
+        title=blog.title, 
+        description=blog.description,
+        owner=blog.owner,
+        read_count=blog.read_count,
+        reading_time=blog.reading_time, 
+        time_stamp=datetime.now().date()  # Keep it as a date object
+    )
+    
+    db.add(blog_info)
+    db.commit()
+    db.refresh(blog_info)
+    
+    return {'id':blog_info.id , 'message':'Blog created'}
+
+@api_router.delete(f'/api/v{VERSION}/del_blog/{{str_id}}')
+def delete_blog(str_id:str, db:Session = Depends(get_db)) :
+    deleted_blog = db.query(Blog).filter(Blog.id==str_id).first()
+    
+    db.delete(deleted_blog)
+    db.commit()
+    return { 'message':'Deleted Blogs'} 
+
+
+@api_router.put(f'/api/v{VERSION}/update_blog/{{str_id}}')
+def update_blog(str_id: str, blog: BlogBase, db: Session = Depends(get_db)):
+    update_blog = db.query(Blog).filter(Blog.id == str_id).first()
+    
+    if update_blog:
+        update_blog.id = blog.id
+        update_blog.title = blog.title
+        update_blog.description = blog.description
+        update_blog.owner = blog.owner
+        update_blog.read_count = blog.read_count
+        update_blog.reading_time = blog.reading_time
+        update_blog.time_stamp = datetime.now().date()
+        
+        db.commit()
+        db.refresh(update_blog)
+        
+        return update_blog
+    
+    return {'id': str_id, 'message': 'Blog Not Found'}
+
